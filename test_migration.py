165c94ce6e0f1b5d31e14c6512791951026b45a4
lemzoo <lemzobba@gmail.com>
2018-02-20 11:50:18 +0100

    test: add test file with empty content

---
+++ test_migration.py
@@ -0,0 +1,12 @@
+class TestMigration:
+
+    def test_nominal_case(self):
+        # Given
+        # Create user
+
+        # When
+        # run migration
+
+        # Then
+        # check the updated user
+        pass
